575d0a4225671a684a93c341064ba96e9d5c08ca
mia0313 <mimicmontrose@gmail.com>
2021-05-01 14:05:25 -0400

    user can save tasks

--- task_app/app.py
+++ task_app/app.py
@@ -1,4 +1,5 @@
-from flask_apscheduler import flask_apscheduler
+from flask import Flask, render_template, current_app, request as app
+from flask_apscheduler import APScheduler
 
 app = Flask(__name__)
 
@@ -8,6 +9,29 @@ scheduler.start()
  
 @app.route('/')
 def index():
+    return render_template('index.html')
+
+
+@app.route('/send', methods=['GET','POST'])
+def sent():
+    #get posteed form data using names assigned in HTML
+    task = request.form['fname']
+    deadline = request.form['deadline']
+    #connect to database and insert task and deadline
+    conn = sqlite3.connect('./static/data/senseDisplay.db')
+    curs = conn.cursor()
+    curs.execute("INSERT INTO tasklist (task, deadline, status) VALUES((?),(?),(?))", (task, deadline, "unstarted"))
+    conn.commit()
+    #close database connection
+    conn.close()
+    #render template with success message
+    return render_template('sent.html', task=task, deadline=deadline)
+
+
+
+'''
+@app.route('/show')
+def all():
     #CONNECT TO db
     conn = sqlite3.connect('./static/data/tasks.db')
     curs = conn.cursor()
@@ -18,7 +42,7 @@ def index():
         tasklist.append(task)
     conn.close()
     return  render_template('index.html', tasklist=tasklist)
-
+'''
 
 
 if __name__ == '__main__':
